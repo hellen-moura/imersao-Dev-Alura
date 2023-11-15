106ed84e23686a9ec4cc0785825a2571e514f457
Hellen Moura <127620071+hellen-moura@users.noreply.github.com>
2023-11-15 19:25:40 -0300

    Delete _conversor-anosluz directory

--- _conversor-anosluz/README.md
+++
@@ -1,4 +0,0 @@
-# _Conversor AnosLuz
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/dkronemberger/pen/jOdGdJx](https://codepen.io/dkronemberger/pen/jOdGdJx).
-
--- _conversor-anosluz/dist/script.js
+++
@@ -1,7 +0,0 @@
-var valorAnosLuz = 3 * 10 ** 8;
-var segundosAnoLuz = 365.25 * 24 * 60 * 60;
-var distEmAnoLuz = Number(prompt("Digite um vamos em Anos Luz: "));
-
-distEmMetrosLuz = distEmAnoLuz * (valorAnosLuz * segundosAnoLuz);
-
-alert("O valor de anos luz para metros é: " + distEmMetrosLuz + " metros");
